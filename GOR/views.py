0059b85bcfe2bb22a457f7496f80b825d637b2f1
Resalfin <resalfin88@gmail.com>
2024-12-17 17:59:28 +0800

    feat: add view to list all fields

--- GOR/views.py
+++ GOR/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .models import Field, Facility
 
 def index(request):
-    return render(request, template_name="index.html")
+    return render(request, "index.html")
+
+
+# Halaman utama dengan daftar nama lapangan
+def render_field_list(request):
+    fields = Field.objects.all()  # Ambil semua field
+    return render(request, 'sportfield/list.html', {
+        'fields': fields  # Kirim data field ke list.html
+    })
+
